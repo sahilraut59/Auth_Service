2c7662e7791229795ebdbaf379439e01d7558f1b
sahilraut59 <sahilraut59@gmail.com>
2023-12-14 14:28:50 +0530

    added jwt verify and create token

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { PORT } = require("./config/serverConfig");
 
 const apiRoutes = require("./routes/index");
 
+const UserService = require("./services/user-service");
+
 const app = express();
 
 const prepareAndStartServer = () => {
